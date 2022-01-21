6b7715be579396076c751ba1f0a15804a72b238b
Mirko <mirkoanna137@gmail.com>
2022-01-21 15:42:37 +0100

    Milestone 1

---
+++ main.js
@@ -0,0 +1,18 @@
+//MILESTONE 1
+
+//Scrivi un programma che stampi in console i numeri da 1 a 100. 
+for (let i = 1; i <=100; i++) {
+    //Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz
+    if ((i % 3 == 0) && (i % 5 == 0)) {
+        console.log("FizzBuzz"); 
+    //Per i multipli di 3 stampi “Fizz” al posto del numero
+    } else if (i % 3 == 0) {
+        console.log("Fizz");
+//Per i multipli di 5 stampi "Buzz" al posto del numero
+    } else if (i % 5 == 0) {
+        console.log("Buzz");
+
+    } else {
+        console.log(i);
+    }
+}
